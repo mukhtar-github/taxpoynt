d65b3b9b7ecb7ee8b9d890ab5c53fc0a46399f7c
Mukhtartg <mukhtartanimugarba@gmail.com>
2024-05-17 08:13:01 +0100

    implement changes in HeaderBox root page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,20 @@
 import HeaderBox from '@/components/HeaderBox'
 
+const loggedIn = {
+  firstName: 'Mukhtar'
+}
+
 const Home = () => {
   return (
     <section className='home'>
       <div className='home-content'>
         <header className='home-header'>
-          <HeaderBox />
+          <HeaderBox
+            type='greeting'
+            title='Welcome'
+            user= {loggedIn?.firstName || 'Guest'}
+            subtext='Access and effectively manage your tax filings for better financial outcomes.'
+          />
         </header>
       </div>
     </section>
