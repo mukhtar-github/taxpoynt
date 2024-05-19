e547dc1fc59f8112075b9960ec96e1273e334429
Mukhtartg <mukhtartanimugarba@gmail.com>
2024-05-19 15:21:13 +0100

    implement changes

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,18 @@
+import Sidebar from "@/components/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = {
+    firstName: 'Mukhtar',
+    lastName: 'Garba'
+  }
+
   return (
-    <main>
-        SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn}/>
         {children}
     </main>
   );
