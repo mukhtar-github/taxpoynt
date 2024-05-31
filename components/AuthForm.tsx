e5b1d36bfbc5c7a9cc881b29493d0058e204fcdb
Mukhtartg <mukhtartanimugarba@gmail.com>
2024-05-31 05:47:35 +0100

    Refactor AuthForm component to use CustomInput for form fields

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -10,9 +10,11 @@ import { Button } from "@/components/ui/button"
 import { Form } from "@/components/ui/form"
 import CustomInput from './CustomInput'
 import { authFormSchema } from '@/lib/utils'
+import { Loader2 } from 'lucide-react'
 
 const AuthForm = ({ type }: { type: string }) => {
     const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(false)
 
     // 1. Define your form.
   const form = useForm<z.infer<typeof authFormSchema>>({
@@ -27,7 +29,9 @@ const AuthForm = ({ type }: { type: string }) => {
   function onSubmit(values: z.infer<typeof authFormSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
+    setLoading(true)
     console.log(values)
+    setLoading(false)
   }
 
   return (
@@ -84,7 +88,16 @@ const AuthForm = ({ type }: { type: string }) => {
                                 type='password'
                             />
                             
-                            <Button type="submit">Submit</Button>
+                            <Button type="submit" className='form-btn' disabled={loading}>
+                                {loading ? (
+                                    <>
+                                        <Loader2
+                                            size={20}
+                                            className='animate-spin'
+                                        /> &nbsp; Loading...
+                                    </>
+                                ) : type === 'sign-in' ? 'Sign In' : 'Sign Up'}
+                            </Button>
                         </form>
                     </Form>
                 </>
