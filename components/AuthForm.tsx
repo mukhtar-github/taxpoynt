6177812280f39bb3356270a6db172930a9550651
Mukhtartg <mukhtartanimugarba@gmail.com>
2024-06-08 17:43:26 +0100

    refactor: Add lazy loading for auth image in RootLayout

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -74,7 +74,7 @@ const AuthForm = ({ type }: { type: string }) => {
             <div className='flex flex-col gap-1 md:gap-3'>
                 <h1 className='tex-24 lg:text-36 font-semibold text-gray-900'>
                     {user 
-                        ? 'Link to dashboard'
+                        ? 'Link Your Account'
                         : type === 'sign-in'
                         ? 'Sign In'
                         : 'Sign Up'
