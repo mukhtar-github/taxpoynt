4582ae36fbd713bf75bc9e0a5e164118b5c0782d
Mukhtartg <mukhtartanimugarba@gmail.com>
2024-06-09 16:52:42 +0100

    refactor: Update Sidebar component to include Footer

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -6,42 +6,42 @@ import { cookies } from "next/headers"
 import { parseStringify } from "../utils"
 
 export const signIn = async ({ email, password }: signInProps) => {
-    try {
-        // Mutation / Database query / Make a fetch request
-        const { account } = await createAdminClient();
+  try {
+    // Mutation / Database query / Make a fetch request
+    const { account } = await createAdminClient();
 
-        const response = await account.createEmailPasswordSession(email, password);
+    const response = await account.createEmailPasswordSession(email, password);
 
-        return parseStringify(response);
+    return parseStringify(response);
 
-    } catch (error) {
-       console.error('Error', error) 
-    }
+  } catch (error) {
+    console.error('Error', error) 
+  }
 }
 
 export const signUp = async (userData: SignUpParams) => {
 
-    const { email, password, firstName, lastName } = userData;
+  const { email, password, firstName, lastName } = userData;
 
-    try {
-        const { account } = await createAdminClient();
+  try {
+    const { account } = await createAdminClient();
 
     const newUserAccount = await account.create(ID.unique(), email, password, `${firstName} ${lastName}`);
   
     const session = await account.createEmailPasswordSession(email, password);
 
     cookies().set("appwrite-session", session.secret, {
-        path: "/",
-        httpOnly: true,
-        sameSite: "strict",
-        secure: true,
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
     });
 
     return parseStringify(newUserAccount);
 
-    } catch (error) {
-       console.error('Error', error) 
-    }
+  } catch (error) {
+    console.error('Error', error) 
+  }
 }
 
 export const getLoggedInUser = async () => {
